a855ee112cd9ac971ede499c7e818470d3b38f1b
Benjamin Lu <b.lu789321@gmail.com>
2024-06-25 00:01:08 +0530

    Initial commit of expense tracker React app

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+//shortcut: rafc gives functional component
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h2>
+      InfoSys InStep Intern Expense Tracker
+    </h2>
+  )
+}
+
+export default Header;
